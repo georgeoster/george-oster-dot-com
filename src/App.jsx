839c9a0d758628ee4890ffca82ca0ec1b75df794
georgeoster <georgeostercodes@gmail.com>
2025-04-19 11:16:01 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='mt-16 text-4xl text-cyan-800 text-center'>Coming Soon...</p>
+    </>
+  )
+}
+
+export default App
