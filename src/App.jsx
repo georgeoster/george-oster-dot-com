6c8c5385f59b3be48577e8b7e5050a8a308873bc
georgeoster <georgeostercodes@gmail.com>
2025-04-20 08:37:44 -0400

    feat:IntroCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import TitleBar from './TitleBar'
+import IntroCard from './IntroCard'
 
 function App() {
 
   return (
     <>
-      <p className='mt-16 text-4xl text-cyan-800 text-center'>Coming Soon...</p>
+      <TitleBar/>
+      <IntroCard/>
     </>
   )
 }
