523b006c1a8e946ca1df414f729441e47c64e335
george oster <georgeostercodes@gmail.com>
2025-05-14 19:41:52 -0400

    feat:AboutMe Items (#7)

---
+++ src/AboutMe/aboutMeCopy.js
@@ -0,0 +1,32 @@
+import charcoalImage from '../assets/charcoal.jpg';
+import meatTowerImage from '../assets/meatTower.jpg';
+import brindleSleepImage from '../assets/brindleSleep.jpg';
+import jojoSmileImage from '../assets/jojoSmile.jpg';
+
+export const charcoal = {
+  title: "Charcoal Only",
+  tagline: "Electric is convenient and pellets are reliable, but flavor favors the brave!",
+  image: charcoalImage,
+  wrapperClass: "order-1 lg:order-1"
+};
+
+export const brindle = {
+  title: "Life of a Dog",
+  tagline: "Brindle spends most of her time recharging for her next walk.",
+  image: brindleSleepImage,
+  wrapperClass: "order-2 lg:order-2"
+};  
+
+export const meatTower = {
+  title: "Meat Tower",
+  tagline: "No spit to rotate your shawarma? No problem! Just rotate the fire instead.",
+  image: meatTowerImage,
+  wrapperClass: "order-3 lg:order-4"
+};  
+
+export const jojoSmile = {
+  title: "The Smiles of JoJo",
+  tagline: "I have never known a happier dog.",
+  image: jojoSmileImage,
+  wrapperClass: "order-4 lg:order-3"
+}; 
