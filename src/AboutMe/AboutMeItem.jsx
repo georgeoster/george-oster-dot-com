8e5cc4b13c19a06533808b36fb7ba669542cf380
george oster <georgeostercodes@gmail.com>
2025-05-19 20:07:52 -0400

    refactor:About Me (#10)

--- src/AboutMe/AboutMeItem.jsx
+++ src/AboutMe/AboutMeItem.jsx
@@ -3,8 +3,8 @@ const AboutMeItem = ({ image, title, tagline }) => {
     <article
       className="
         group grid grid-cols-2 gap-2 p-2 
-        bg-neutral-800 rounded-3xl border-2 border-neutral-700 
-        hover:border-sky-800 hover:shadow-lg hover:scale-[1.01] 
+        bg-neutral-50 rounded-3xl border border-neutral-200 
+        hover:shadow-md hover:scale-[1.01] 
         transition-all duration-300 ease-in-out cursor-pointer
       "
     >
@@ -14,14 +14,15 @@ const AboutMeItem = ({ image, title, tagline }) => {
         className="w-full h-40 sm:h-48 md:h-56 lg:h-64 rounded-2xl object-cover"
       />
       <div className="flex flex-col justify-center pl-2">
-        <h3 className="text-neutral-300 text-md md:text-lg lg:text-xl font-bold transition-colors duration-300">
+        <h3 className="text-neutral-800 text-md md:text-lg lg:text-xl font-bold transition-colors duration-300">
           {title}
         </h3>
-        <p className="text-neutral-500 text-xs md:text-sm lg:text-md mt-2 transition-colors duration-300">
+        <p className="text-neutral-600 text-xs md:text-sm lg:text-md mt-2 transition-colors duration-300">
           {tagline}
         </p>
       </div>
     </article>
   );
-}
-export default AboutMeItem
+};
+
+export default AboutMeItem;
