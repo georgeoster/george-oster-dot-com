7bf0d83b35513b1cc64204e38c6cf645f63ae1b8
george oster <georgeostercodes@gmail.com>
2025-05-21 16:41:22 -0400

    refactor:AboutMe (#12)

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import AboutMeItem from "./AboutMeItem";
 import SectionContainer from "../Shared/SectionContainer";
+import AboutMeModal from "./AboutMeModal";
 import { charcoal, brindle, meatTower, jojoSmile } from "./aboutMeCopy";
 
 const AboutMe = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = (item) => {
+    setSelectedItem(item);
+    setShowModal(true);
+  };
+
   const items = [charcoal, brindle, meatTower, jojoSmile];
 
   return (
     <section className="bg-white py-4 md:py-6 lg:py-8 text-neutral-800">
       <SectionContainer>
+        <AboutMeModal
+          isOpen={showModal}
+          item={selectedItem}
+          onClose={() => setShowModal(false)}
+        />
+
         <h2 className="relative text-center text-2xl md:text-3xl lg:text-4xl font-bold text-neutral-800 mb-8 after:content-[''] after:block after:h-1 after:w-16 after:mx-auto after:mt-2 after:bg-cyan-800 after:rounded-full">
           Life Outside the Laptop
         </h2>
@@ -19,6 +35,7 @@ const AboutMe = () => {
                 image={item.image}
                 title={item.title}
                 tagline={item.tagline}
+                onClick={() => openModal(item)}
               />
             </div>
           ))}
--- src/AboutMe/aboutMeCopy.js
+++ src/AboutMe/aboutMeCopy.js
@@ -2,6 +2,30 @@ import charcoalImage from '../assets/charcoal.jpg';
 import meatTowerImage from '../assets/meatTower.jpg';
 import brindleSleepImage from '../assets/brindleSleep.jpg';
 import jojoSmileImage from '../assets/jojoSmile.jpg';
+import baby from '../assets/brindle/baby.png'
+import sheds from '../assets/brindle/sheds.jpg'
+import smile from '../assets/brindle/smile.jpg'
+import backyard from '../assets/brindle/backyardwithjojo.jpg'
+import watchful from '../assets/brindle/watchful.jpg'
+import kisses from '../assets/brindle/kisses.jpg'
+import goodsleep from '../assets/brindle/goodsleep.jpg'
+import xmasbone from '../assets/brindle/xmasbone.jpg'
+import xmassleep from '../assets/brindle/xmassleep.jpg'
+import iamcute from '../assets/brindle/iamcute.jpg'
+import bigsmile from '../assets/jojo/bigsmile.jpg'
+import blanketsleep from '../assets/jojo/blanketsleep.jpg'
+import happypant from '../assets/jojo/happypant.jpg'
+import happysit from '../assets/jojo/happysit.jpg'
+import hoodiesleep from '../assets/jojo/hoodiesleep.jpg'
+import innocencetwo from '../assets/jojo/innocencetwo.jpg'
+import meandjojo from '../assets/jojo/meandjo.jpg'
+import sillyroach from '../assets/jojo/sillyroach.jpg'
+import sleeplikeaperson from '../assets/jojo/sleeplikeaperson.jpg'
+import snuggly from '../assets/jojo/snuggly.jpg'
+import snugglyalert from '../assets/jojo/snugglyalert.jpg'
+import sonoble from '../assets/jojo/sonoble.jpg'
+import weirdsleep from '../assets/jojo/weirdsleep.jpg'
+import xmasbonejojo from '../assets/jojo/xmasbone.jpg'
 
 export const charcoal = {
   title: "Charcoal Only",
@@ -14,7 +38,19 @@ export const brindle = {
   title: "Life of a Dog",
   tagline: "Brindle spends most of her time recharging for her next walk.",
   image: brindleSleepImage,
-  wrapperClass: "order-2 lg:order-2"
+  wrapperClass: "order-2 lg:order-2",
+  photos: [
+    { image: baby, caption: "She used to be so young" },
+    { image: sheds, caption: "When she sheds, she SHEDS" },
+    { image: smile, caption: "That smile though!" },
+    { image: backyard, caption: "She pretends to not care about JoJo" },
+    { image: watchful, caption: "But she aint foolin no one" },
+    { image: kisses, caption: "She has been known to sneaky kiss" },
+    { image: goodsleep, caption: "But mostly she just likes to sleep" },
+    { image: xmasbone, caption: "She gets a bone for Christmas every year" },
+    { image: xmassleep, caption: "Which just leads to more sleep time" },
+    { image: iamcute, caption: "I feel like she knows how cute she is" }
+  ]
 };  
 
 export const meatTower = {
@@ -28,5 +64,20 @@ export const jojoSmile = {
   title: "The Smiles of JoJo",
   tagline: "I have never known a happier dog.",
   image: jojoSmileImage,
-  wrapperClass: "order-4 lg:order-3"
+  wrapperClass: "order-4 lg:order-3",
+  photos: [
+    { image: bigsmile, caption: 'Always smiling'},
+    { image: blanketsleep, caption: 'Sometimes she is sleepy'},
+    { image: happypant, caption: 'She loves her sunshine'},
+    { image: happysit, caption: 'Sit like a person!'},
+    { image: hoodiesleep, caption: 'Stealer of hoodies!'},
+    { image: innocencetwo, caption: 'Full on puppy dog eyes'},
+    { image: meandjojo, caption: 'Any excuse to cuddle'},
+    { image: sillyroach, caption: 'Belly to the sky!'},
+    { image: sleeplikeaperson, caption: 'This how dad sleeps! I am person!'},
+    { image: snuggly, caption: 'She LOVES to be swaddled'},
+    { image: sonoble, caption: 'caption'},
+    { image: weirdsleep, caption: 'Occasionally she becomes a Non-Newtonian solid'},
+    { image: xmasbonejojo, caption: 'Christmas means marrow bones'},
+  ]
 }; 
