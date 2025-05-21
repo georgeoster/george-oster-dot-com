f529cf1e89ee7e985b7fc7880a5fb490e4170757
georgeoster <georgeostercodes@gmail.com>
2025-05-21 17:57:42 -0400

    cleanup:AboutMe

--- src/AboutMe/aboutMeCopy.js
+++ src/AboutMe/aboutMeCopy.js
@@ -1,7 +1,5 @@
-import charcoalImage from '../assets/charcoal.jpg';
-import meatTowerImage from '../assets/meatTower.jpg';
-import brindleSleepImage from '../assets/brindleSleep.jpg';
-import jojoSmileImage from '../assets/jojoSmile.jpg';
+//brindle
+import brindleSleepImage from '../assets/brindle/brindleSleep.jpg';
 import baby from '../assets/brindle/baby.png'
 import sheds from '../assets/brindle/sheds.jpg'
 import smile from '../assets/brindle/smile.jpg'
@@ -12,6 +10,8 @@ import goodsleep from '../assets/brindle/goodsleep.jpg'
 import xmasbone from '../assets/brindle/xmasbone.jpg'
 import xmassleep from '../assets/brindle/xmassleep.jpg'
 import iamcute from '../assets/brindle/iamcute.jpg'
+//jojo
+import jojoSmileImage from '../assets/jojo/jojoSmile.jpg';
 import bigsmile from '../assets/jojo/bigsmile.jpg'
 import blanketsleep from '../assets/jojo/blanketsleep.jpg'
 import happypant from '../assets/jojo/happypant.jpg'
@@ -25,6 +25,8 @@ import snuggly from '../assets/jojo/snuggly.jpg'
 import sonoble from '../assets/jojo/sonoble.jpg'
 import weirdsleep from '../assets/jojo/weirdsleep.jpg'
 import xmasbonejojo from '../assets/jojo/xmasbone.jpg'
+//charcoal
+import charcoalImage from '../assets/charcoal/charcoal.jpg';
 import charcoaldump from '../assets/charcoal/charcoaldump.jpg'
 import chuckcooked from '../assets/charcoal/chuckcooked.jpg'
 import chuckraw from '../assets/charcoal/chuckraw.jpg'
@@ -33,11 +35,12 @@ import cookedsaucedribs from '../assets/charcoal/cookedsaucedribs.jpg'
 import fajitas from '../assets/charcoal/fajitas.jpg'
 import rawsmokeyribs from '../assets/charcoal/rawsmokeyribs.jpg'
 import saucetheribs from '../assets/charcoal/saucetheribs.jpg'
+//meattower
+import meatTowerImage from '../assets//meattower/meatTower.jpg';
 import almostdone from '../assets/meattower/almostdone.jpg'
 import finishedinside from '../assets/meattower/finishedinside.jpg'
 import rawinside from '../assets/meattower/rawinside.jpg'
 import rawstart from '../assets/meattower/rawstart.jpg'
-import sliced from '../assets/meattower/sliced.jpg'
 import snake from '../assets/meattower/snake.jpg'
 
 export const charcoal = {
@@ -46,13 +49,13 @@ export const charcoal = {
   image: charcoalImage,
   wrapperClass: "order-1 lg:order-1",
   photos: [
-    { image: charcoaldump, caption: 'Charcoal or Nothin!' },
-    { image: rawsmokeyribs, caption: 'I love this rib holder' },
-    { image: closeupcookedribsinrack, caption: 'caption' },
-    { image: saucetheribs, caption: 'Sauce the Ribs' },
-    { image: cookedsaucedribs, caption: 'Arguably what I am best at' },
-    { image: chuckraw, caption: 'Chuck Roast with smokey ketchup' },
-    { image: chuckcooked, caption: 'Came out pretty good' },
+    { image: charcoaldump, caption: 'Charcoal or Bust!' },
+    { image: rawsmokeyribs, caption: 'Rib holder ... testing' },
+    { image: closeupcookedribsinrack, caption: 'Rib holder ... good purchase' },
+    { image: saucetheribs, caption: 'Sauce' },
+    { image: cookedsaucedribs, caption: 'Ribs are my strong suit' },
+    { image: chuckraw, caption: 'Chuck Roast with Smokey Ketchup' },
+    { image: chuckcooked, caption: 'Better than I thought' },
     { image: fajitas, caption: 'Fun Fact: Fajitas can be smoked' },
   ]
 };
@@ -87,7 +90,6 @@ export const meatTower = {
     { image: rawstart, caption: 'and so it begins' },
     { image: almostdone, caption: 'i can taste this picture' },
     { image: finishedinside, caption: '...i mean ...' },
-    { image: sliced, caption: 'caption' },
   ]
 };  
 
@@ -97,18 +99,18 @@ export const jojoSmile = {
   image: jojoSmileImage,
   wrapperClass: "order-4 lg:order-3",
   photos: [
-    { image: bigsmile, caption: 'Always smiling'},
-    { image: blanketsleep, caption: 'Sometimes she is sleepy'},
-    { image: happypant, caption: 'She loves her sunshine'},
-    { image: happysit, caption: 'Sit like a person!'},
-    { image: hoodiesleep, caption: 'Stealer of hoodies!'},
-    { image: innocencetwo, caption: 'Full on puppy dog eyes'},
-    { image: meandjojo, caption: 'Any excuse to cuddle'},
+    { image: bigsmile, caption: 'Hello friend!'},
+    { image: happypant, caption: 'Sun is good'},
+    { image: happysit, caption: 'Doin a human sit'},
+    { image: blanketsleep, caption: 'I sleep now'},
+    { image: sonoble, caption: 'This my thinking face'},
+    { image: hoodiesleep, caption: 'I steal dads hoodies!'},
+    { image: innocencetwo, caption: 'You cannot resist my cuteness'},
+    { image: meandjojo, caption: 'Doin a heckin cuddle'},
     { image: sillyroach, caption: 'Belly to the sky!'},
     { image: sleeplikeaperson, caption: 'This how dad sleeps! I am person!'},
-    { image: snuggly, caption: 'She LOVES to be swaddled'},
-    { image: sonoble, caption: 'caption'},
-    { image: weirdsleep, caption: 'Occasionally she becomes a Non-Newtonian solid'},
+    { image: snuggly, caption: 'SWADDLE ME!!!'},
+    { image: weirdsleep, caption: 'If I fits I sits'},
     { image: xmasbonejojo, caption: 'Christmas means marrow bones'},
   ]
 }; 
